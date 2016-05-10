ce4f97d7f885fa321d9b7ebb6449fc5bad1c3b41
ZenStein <shaneenterprises@gmail.com>
2016-05-10 13:06:38 -0700

    commiting

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+echo 'here here here'
+#var codeData = require('/Users/ShahinPro/.csScripts/commands')
+
+#var terminalCommands = new codeData.Commands()
+	
+#terminalCommands.getCommands('find')
